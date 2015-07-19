98738eabbc071e4a467c1fcd02b791aa75ea44f0
Dylan Nissley <dylan.nissley@gmail.com>
2015-07-19 10:45:20 -0400

    C++ Primer: More struct exercises and header file intro

---
+++ primer/exercise-2.42/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Sales_data.h"
+
+int main()
+{
+    Sales_data total; // variable to hold data for the next transaction
+    // read the first transaction and ensure that there are data to process
+    double price = 0.0;
+    if (std::cin >> total.bookNo >> total.units_sold >> price) {
+        total.revenue = total.units_sold * price;
+        Sales_data trans; // variable to hold the running sum
+        // read and process the remaining transactions
+        while (std::cin >> trans.bookNo >> trans.units_sold >> price) {
+            trans.revenue = trans.units_sold * price;
+            // if we're still processing the same book
+            if (total.bookNo == trans.bookNo) {
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue; // update the running total
+            }
+            else {
+                // print results for the previous book
+                std::cout << total.bookNo << " " << total.units_sold << " " << total.revenue << " " << total.revenue / total.units_sold << std::endl;
+                total = trans;  // total now refers to the next book
+            }
+        }
+        std::cout << total.bookNo << " " << total.units_sold << " " << total.revenue << " " << total.revenue / total.units_sold << std::endl; // print the last transaction
+    } else {
+        // no input! warn the user
+        std::cerr << "No data?!" << std::endl;
+        return -1;  // indicate failure
+    }
+    return 0;
+}
